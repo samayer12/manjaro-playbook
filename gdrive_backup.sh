892a1cf7bd70ae24c8ba9c783469981351f7d464
sam <samayer12@gmail.com>
2021-01-12 15:19:14 -0500

    Use verbose uploading

--- gdrive_backup.sh
+++ gdrive_backup.sh
@@ -15,5 +15,5 @@
 # crontab
 # */x * * * * /path/to/file/gdrive_backup.sh
 
-/usr/bin/rclone copy remote: ~/gdrive
+/usr/bin/rclone copy --update --verbose --transfers 30 --checkers 8 --contimeout 60s --timeout 300s --retries 3 --low-level-retries 10 --stats 1s --log-file=/var/log/rclone_gd.log remote: ~/gdrive 
 /usr/bin/rclone copy --update --verbose --transfers 30 --checkers 8 --contimeout 60s --timeout 300s --retries 3 --low-level-retries 10 --stats 1s --log-file=/var/log/rclone_gd.log ~/gdrive remote: 
