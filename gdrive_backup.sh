f9dacfe1608422d0047a6a3edc57d0dc275e5e08
sam <samayer12@gmail.com>
2021-01-12 14:49:07 -0500

    Add ref to loggin solution

--- gdrive_backup.sh
+++ gdrive_backup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+# Logging approach from here: https://github.com/rclone/rclone/issues/2259
 
 /usr/bin/rclone copy --update --verbose --transfers 30 --checkers 8 --contimeout 60s --timeout 300s --retries 3 --low-level-retries 10 --stats 1s --log-file=/var/log/rclone_gd.log ~/gdrive remote: 
