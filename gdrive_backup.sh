eedf8035c53eee9b9c68ff36905b38c8af127c36
sam <samayer12@gmail.com>
2021-01-12 14:41:15 -0500

    Change logging path

--- gdrive_backup.sh
+++ gdrive_backup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/usr/bin/rclone copy --update --verbose --transfers 30 --checkers 8 --contimeout 60s --timeout 300s --retries 3 --low-level-retries 10 --stats 1s --log-file=/var/log/rclone/gdrive.log ~/gdrive remote: 
+/usr/bin/rclone copy --update --verbose --transfers 30 --checkers 8 --contimeout 60s --timeout 300s --retries 3 --low-level-retries 10 --stats 1s --log-file=/var/log/rclone_gd.log ~/gdrive remote: 
