d1d6aa0348cc9fc40489b610c0226d78031762a3
sam <samayer12@gmail.com>
2021-01-12 20:44:58 -0500

    Use filtering file

--- gdrive_backup.sh
+++ gdrive_backup.sh
@@ -16,7 +16,7 @@
 # */x * * * * /path/to/file/gdrive_backup.sh
 
 echo "------STARTING PULL------" >> /var/log/rclone_gd.log
-/usr/bin/rclone copy --update --verbose --transfers 30 --checkers 8 --contimeout 60s --timeout 300s --retries 3 --low-level-retries 10 --stats 1s --log-file=/var/log/rclone_gd.log remote: ~/gdrive --exclude *venv*
+/usr/bin/rclone copy --update --verbose --transfers 30 --checkers 8 --contimeout 60s --timeout 300s --retries 3 --low-level-retries 10 --stats 1s --log-file=/var/log/rclone_gd.log remote: ~/gdrive --exclude-from rclone_filter.txt
 echo "------ STARTING PUSH------" >> /var/log/rclone_gd.log
-/usr/bin/rclone copy --update --verbose --transfers 30 --checkers 8 --contimeout 60s --timeout 300s --retries 3 --low-level-retries 10 --stats 1s --log-file=/var/log/rclone_gd.log ~/gdrive remote: --exclude *venv*
+/usr/bin/rclone copy --update --verbose --transfers 30 --checkers 8 --contimeout 60s --timeout 300s --retries 3 --low-level-retries 10 --stats 1s --log-file=/var/log/rclone_gd.log ~/gdrive remote: --exclude-from rclone_filter.txt
  
