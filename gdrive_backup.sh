dbf076a50878a826c21019964a27dbbb348ece33
sam <samayer12@gmail.com>
2021-01-12 20:32:58 -0500

    Exclude Python venv

--- gdrive_backup.sh
+++ gdrive_backup.sh
@@ -16,6 +16,7 @@
 # */x * * * * /path/to/file/gdrive_backup.sh
 
 echo "------STARTING PULL------" >> /var/log/rclone_gd.log
-/usr/bin/rclone copy --update --verbose --transfers 30 --checkers 8 --contimeout 60s --timeout 300s --retries 3 --low-level-retries 10 --stats 1s --log-file=/var/log/rclone_gd.log remote: ~/gdrive 
+/usr/bin/rclone copy --update --verbose --transfers 30 --checkers 8 --contimeout 60s --timeout 300s --retries 3 --low-level-retries 10 --stats 1s --log-file=/var/log/rclone_gd.log remote: ~/gdrive --exclude *venv*
 echo "------ STARTING PUSH------" >> /var/log/rclone_gd.log
-/usr/bin/rclone copy --update --verbose --transfers 30 --checkers 8 --contimeout 60s --timeout 300s --retries 3 --low-level-retries 10 --stats 1s --log-file=/var/log/rclone_gd.log ~/gdrive remote: 
+/usr/bin/rclone copy --update --verbose --transfers 30 --checkers 8 --contimeout 60s --timeout 300s --retries 3 --low-level-retries 10 --stats 1s --log-file=/var/log/rclone_gd.log ~/gdrive remote: --exclude *venv*
+ 
